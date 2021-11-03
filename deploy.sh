9f461ad0d97614e8075a66e685673d5a40368708
Anna <cameaann@gmail.com>
2021-11-04 00:02:03 +0300

    build.1

--- deploy.sh
+++ deploy.sh
@@ -12,14 +12,14 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
+# git init
+# git add -A
+# git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:<cameaann>/<anna-sh>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:<cameaann>/<anna-sh>.git main:gh-pages
 
 cd -
